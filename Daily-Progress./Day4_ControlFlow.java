c6c9d763c722d4ea9d07dd35a0a62822228973f3
darshan02parmar <darshan02parmar@gmail.com>
2024-11-08 00:14:41 +0530

    Create Day4_ControlFlow.java

---
+++ Daily-Progress./Day4_ControlFlow.java
@@ -0,0 +1,90 @@
+
+// check the number is Prime or not
+import java.util.*;
+public class Prime {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+
+        System.out.println("Enter a number:");
+        int n=s.nextInt();
+        if(n==2){
+            System.out.println("n is prime");
+        }
+        else{
+            boolean isPrime=true;
+            for(int i=2;i<Math.sqrt(n);i++){
+                if(n%i==0){
+                    isPrime=false;
+                }
+            }
+            if(isPrime==true){
+                System.out.println("n is prime");
+            }else{
+                System.out.println("n is not prime");
+            }
+        }
+    }
+}
+
+//Printing the Reverse of a Number
+public class Print_reverse {
+    public static void main(String[] args) {
+        int n = 10392;
+
+        while (n > 0) {
+            int lastDigit = n % 10;
+            System.out.print(lastDigit);
+            n /= 10;
+        }
+        System.out.println();
+    }
+}
+
+//Reversing the Given Number
+public class Reverse_givenNumber {
+    public static void main(String[] args) {
+        int rev = 0;
+        int n = 91025;
+
+        while (n > 0) {
+            int lastDigit = n % 10;
+            rev = (rev * 10) + lastDigit;
+            n /= 10;
+        }
+        System.out.println(rev);
+    }
+}
+
+//total sum of the even and odd integers
+
+import java.util.Scanner;
+
+public class SumEvenOdd {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int evenSum = 0, oddSum = 0;
+
+        System.out.println("Enter integers (type '0' to end):");
+
+        while (true) {
+            int num = scanner.nextInt();
+
+            // Exit loop if the user inputs 0
+            if (num == 0) {
+                break;
+            }
+
+            // Check if the number is even or odd and add to respective sum
+            if (num % 2 == 0) {
+                evenSum += num;
+            } else {
+                oddSum += num;
+            }
+        }
+
+        // Display the results
+        System.out.println("Sum of even numbers: " + evenSum);
+        System.out.println("Sum of odd numbers: " + oddSum);
+    }
+}
+//
