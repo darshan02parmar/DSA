f20d298c5d04c95c5b947d58364cbfefae11a402
Darshan Parmar <darshan02parmar@gmail.com>
2024-11-13 23:16:24 +0530

    Update Day7_Function.java

--- Daily-Progress./Day7_Function.java
+++ Daily-Progress./Day7_Function.java
@@ -18,3 +18,27 @@ public class Func_sum {
         System.out.println("sum of a and b is: "+add);
     }
  }
+
+//Program to find sum of first n natural numbers
+public class SumOfNaturalNumbers {
+
+    // Function to calculate sum of first n natural numbers
+    public static int sumOfNaturalNumbers(int n) {
+        return n * (n + 1) / 2;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a positive integer: ");
+        int n = scanner.nextInt();
+
+        if (n > 0) {
+            int sum = sumOfNaturalNumbers(n);
+            System.out.println("The sum of the first " + n + " natural numbers is: " + sum);
+        } else {
+            System.out.println("Please enter a positive integer.");
+        }
+
+        scanner.close();
+    }
+}
