f8c0769ee7014f0072e67b608a53d08bc23e0f0e
Darshan Parmar <darshan02parmar@gmail.com>
2024-11-14 22:32:34 +0530

    Update Day7_Function.java

--- Daily-Progress./Day7_Function.java
+++ Daily-Progress./Day7_Function.java
@@ -42,3 +42,88 @@ public class SumOfNaturalNumbers {
         scanner.close();
     }
 }
+
+//swapping
+public class Swap {
+
+    public static void swap(int a, int b){
+
+        int temp=a;
+        a=b;
+        b=temp;
+        System.out.println("a="+a+  "b="+b);
+    }
+    public static void main(String[] args) {
+        
+        int a=9;
+        int b=2;
+        swap(a, b);    
+    }
+}
+
+//Multiplication 
+public class Product {
+
+    public static int factorial(int n){
+        int f=1;
+
+        for (int i = 1; i<=n; i++) {
+            f=f*i;
+        }
+        return f;
+    }
+
+    public static int multiply(int a,int b){
+        int product=a*b;
+        return product;
+    }
+    public static void main(String[] args) {
+        int a=8;
+        int b=10;
+        int prod=multiply(a, b);
+        System.out.println("Multiplication is:"+prod);
+    }
+
+}
+//Factorial
+public class Fact {
+
+
+    public static int factorial(int n){
+        int f=1;
+
+        for (int i = 1; i<=n; i++) {
+            f=f*i;
+        }
+        return f;
+    }
+    public static void main(String[] args) {
+        
+        System.out.println("Factorial is: "+factorial(5));
+    }
+
+}
+
+//Bionominal coefficient 
+class Bionominal{
+    public static int factorial(int n){
+        int f=1;
+
+        for (int i = 1; i<=n; i++) {
+            f=f*i;
+        }
+        return f;
+    }
+public static int BinoCoofi(int n,int r){
+    int fact_n=factorial(n);
+    int fact_r=factorial(r);
+    int fact_nmr=factorial(n-r);
+
+    int biocoffi=fact_n/(fact_r*fact_nmr);
+    return biocoffi;
+}
+public static void main(String[] args) {
+    System.out.println(BinoCoofi(5, 2));
+}
+
+}
