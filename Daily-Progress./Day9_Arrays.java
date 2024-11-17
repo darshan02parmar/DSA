924e9226b75b74d0834e6195db87b644b0628d41
Darshan Parmar <darshan02parmar@gmail.com>
2024-11-17 12:49:58 +0530

    Update Day9_Arrays.java
    Array[i]

--- Daily-Progress./Day9_Arrays.java
+++ Daily-Progress./Day9_Arrays.java
@@ -159,4 +159,50 @@ public class Reverse {
         System.out.println();
     }
 }
+//Pairs in array
+public class Pairs {
+    public static void pairsArray(int numbers[]){
+        int tp=0;
+        for (int i = 0; i <numbers.length ; i++) {
+            int current=numbers[i];
+            for (int j = i+1; j < numbers.length; j++) {
+                System.out.print("("+current + ","+numbers[j] +")");
+                tp++;
+            }
+            System.out.println();
+        }
+        System.out.println("total pairs= "+tp);
+    }
+    public static void main(String[] args) {
+            int[] numbers={2,4,6,8,10};
+            pairsArray(numbers);
+    }
+}
+
+// print Sub array
+
+public class SubArray {
+
+    public static void printsubArray(int numbers[]){
+        int ts=0;
+        for (int i = 0; i < numbers.length ; i++) {
+            int start=i;
+            for (int j = i; j < numbers.length ; j++) {
+                int end=j;
+                for (int k = start; k <=end ; k++) {
+                    System.out.print(numbers[k]+" ");
+                }
+                ts++;
+                System.out.println();
+            }
+            System.out.println();
+        }
+        System.out.println("total sub array = "+ts);
+    }
+
+    public static void main(String[] args) {
+        int[] numbers={2,4,6,8,10};
+        printsubArray(numbers);
+    }
+}
 
