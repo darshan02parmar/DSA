72a64d838ea47eb4473a1aad131955ed27b119c4
Darshan Parmar <darshan02parmar@gmail.com>
2024-11-17 12:16:39 +0530

    Update Day9_Arrays.java

--- Daily-Progress./Day9_Arrays.java
+++ Daily-Progress./Day9_Arrays.java
@@ -132,4 +132,31 @@ public class Largest {
         System.out.println("largest number is: "+getLargest(numbers));
     }
 }
+//Reverse Array
+public class Reverse {
+
+    public static void revrseArray(int numbers[]){
+        int first=0;
+        int last= numbers.length-1;
+
+        while (first<last){
+
+            int temp=numbers[last];
+            numbers[last]=numbers[first];
+            numbers[first]=temp;
+
+            first++;
+            last--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] numbers={2,4,6,8,10};
+        revrseArray(numbers);
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i]+" ");
+        }
+        System.out.println();
+    }
+}
 
