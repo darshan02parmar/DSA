fbe93d41aba0e459a8429b6498212ce9e8652099
Darshan Parmar <darshan02parmar@gmail.com>
2024-11-19 11:57:00 +0530

    Update Day11_MaxSubArraySum.java
    Maximum Subarray Sum in an Array [ All 3 methods ]

--- Daily-Progress./Day11_MaxSubArraySum.java
+++ Daily-Progress./Day11_MaxSubArraySum.java
@@ -54,6 +54,23 @@ public class MaxsubarraySum {
         System.out.println("max sum of sub array = " + maxSum);
     }
 
+  //3] // Kadanes Algorithm
+
+    public static void kadanes(int numbers[]){
+
+        int ms=Integer.MIN_VALUE;
+        int cs=0;
+
+        for (int i = 0; i < numbers.length ; i++) {
+            cs=cs+numbers[i];
+            if (cs<0) {
+                cs=0;
+            }
+            ms=Math.max(ms,cs);
+        }
+        System.out.println("max sum of sub array = " + ms);
+    }
+
     public static void main(String[] args) {
         int[] numbers={-2,-3,4,-2,-1,1,5,-3};
         kadanes(numbers);
