0f39cb776590d19c18f6ebb690dc081615f81c40
Darshan Parmar <darshan02parmar@gmail.com>
2024-11-23 22:49:29 +0530

    Day15_2D_Arrays.java

--- Daily-Progress./Day15_2D_Arrays.java
+++ Daily-Progress./Day15_2D_Arrays.java
@@ -38,3 +38,55 @@ public class Creation {
     }
 }
 
+//2] Largest And Smallest
+
+import java.util.Scanner;
+public class Largest_Smallest {
+
+    public static  int findlargest(int[][] matrix){
+        int largest=Integer.MIN_VALUE;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if(matrix[i][j]>largest) {
+                    largest=matrix[i][j];
+                }
+            }
+        }
+        return largest;
+    }
+    public static int findsmallest(int[][] matrix){
+        int smallest=Integer.MAX_VALUE;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if(matrix[i][j]<smallest) {
+                    smallest=matrix[i][j];
+                }
+            }
+        }
+        return smallest;
+    }
+    public static void main(String[] args) {
+        int matrix[][] = new int[3][3];
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print("Enter Values: ");
+                Scanner s = new Scanner(System.in);
+                matrix[i][j] = s.nextInt();
+            }
+        }
+        //print
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("largest element="+findlargest(matrix));
+        System.out.println("smallest element="+findsmallest(matrix));
+
+    }
+}
+
