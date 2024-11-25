dd1d18b47ce2306578dec66a8935f760a35de87e
Darshan Parmar <darshan02parmar@gmail.com>
2024-11-25 22:32:07 +0530

    Update Day17_2D(problems).java

--- Daily-Progress./Day17_2D(problems).java
+++ Daily-Progress./Day17_2D(problems).java
@@ -21,3 +21,25 @@ public class MatrixDiagonalSum {
         System.out.println(diagonalSum(matrix));
     }
 }
+
+// Search in 2D matrix
+
+public class SearchInMatrix {
+    public static boolean serach(int[][] matrix,int target){
+        int row=0 , col= matrix[0].length-1;
+
+        while (row < matrix.length && col>=0){
+            if (matrix[row][col]==target) return true;
+            else if (matrix[row][col]<target) row++;
+            else col--;
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int[][] matrix={{1,2,3,4},
+                {5,6,7,8},
+                {9,10,11,12},
+                {13,14,15,16}};
+        System.out.println(serach(matrix,8));
+    }
+}
