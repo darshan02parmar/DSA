91f827067e239b7cbcea12c3a2d56b3ceb85c96f
Darshan Parmar <darshan02parmar@gmail.com>
2024-11-26 13:15:44 +0530

    Update and rename Daily-Progress./Day18_Strings.java to Daily-Progress./Day18_Strings.java./Palindrome.java

--- Daily-Progress./Day18_Strings.java./Palindrome.java
+++ Daily-Progress./Day18_Strings.java./Palindrome.java
@@ -66,3 +66,23 @@ public class Basics {
         System.out.println(str.substring(0,3));
     }
 }
+
+// Palindrome string
+public class Palindrome {
+
+    public static boolean ispalindrome(String str){
+
+        for (int i = 0; i <str.length()/2 ; i++) {
+            int n=str.length();
+            if (str.charAt(i) != str.charAt(n-1-i)) {
+                //not palindrome
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        String str="racecar";
+        System.out.println(ispalindrome(str));
+    }
+}
