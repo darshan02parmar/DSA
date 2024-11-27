c83526c8394db5f38a36299e59e72d9261f0c2ba
Darshan Parmar <darshan02parmar@gmail.com>
2024-11-27 23:23:50 +0530

    Update and rename Day17_String(problems).java to Day19_String(problems).java

--- Daily-Progress./Day19_String(problems).java
+++ Daily-Progress./Day19_String(problems).java
@@ -50,3 +50,36 @@ public class String_Compression {
         System.out.println(compress(str));
     }
 }
+//Anagrams
+
+import java.util.*;
+public class Anagrams {
+    public static void main(String[] args) {
+        String str1="race";
+        String str2="care";
+
+//Convert Strings to lowercase. Why? so that we don't have to check separately for lower & uppercase
+        str1=str1.toLowerCase();
+        str2=str2.toLowerCase();
+
+        if(str1.length()==str2.length()){
+
+            // convert strings into char array
+            char[] str1charArray=str1.toCharArray();
+            char[] str2charArray=str2.toCharArray();
+
+            Arrays.sort(str1charArray);
+            Arrays.sort(str2charArray);
+
+            // if the sorted char arrays are same or identical then the strings are areanagram
+            boolean result=Arrays.equals(str1charArray,str2charArray);
+            if(result){
+                System.out.println(str1+" and "+str2+" are anagram of eachother");
+            }else {
+                System.out.println(str1+" and "+str2+" are not anagram of eachother");
+            }
+        }else {
+            System.out.println(str1+" and "+str2+" are not anagram of eachother");
+        }
+    }
+}
